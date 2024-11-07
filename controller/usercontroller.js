e6f02aa744bdbb283b4b325afb1b127fa27cbd5b
Sharon812 <sharonsharu1110@gmail.com>
2024-11-07 11:31:08 +0530

    added landing page,password hashed

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -1,4 +1,16 @@
 const users = require('../models/User')
+const bycrypt = require('bcrypt')
+
+//converting into a hash string
+const securepassword = async (password) =>{
+    try {
+        const hashpass = await bycrypt.hash(password,10);
+        return hashpass;
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+//registration page rendering
 const loadregistrationpage = async(req,res) => {
     try{
         res.render('registration')
@@ -8,24 +20,44 @@ const loadregistrationpage = async(req,res) => {
     }
 }
 
+//to insert user
 const insertUser = async(req,res) => {
     try {
-        const { name, email, password } = req.body;
-        const user = new users({ name, email, password });
+        //bycrypt using
+        const secpass = await securepassword(req.body.password)
+        //inserting user
+        const user = new users({
+            name:req.body.name,
+            email:req.body.email,
+            password:secpass
+        })
 
+
+        //saving data to databasse
         const userdata = await user.save();
         console.log(userdata)
         if(userdata){
-            res.render('registration',{message:"Your registration is success"})
+            res.render('registration')
         }else{
-            res.render('registration',{message:"YOur registrationn is faiked"})
+            res.render('registration')
         }
     } catch (error) {
         console.log(error.message)
     }
 }
 
+//Home 1rst page rendering
+const landingpage = async (req,res) => {
+    try {
+        res.render('landingpage')
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+//exporting
 module.exports ={
     loadregistrationpage,
-    insertUser
+    insertUser,
+    landingpage
 } 
--- routes/userroute.js
+++ routes/userroute.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const user_route = express();
 
+//setting view engine
 user_route.set('view engine','ejs')
 user_route.set('views','./views/users')
 
-
+//initialising bodyparser
 const bodyparser = require('body-parser')
 user_route.use(bodyparser.json())
 user_route.use(bodyparser.urlencoded({extended:true}))
 
 const usercontroller = require('../controller/usercontroller');
 
+//register route
 user_route.get('/register',usercontroller.loadregistrationpage);
 user_route.post('/register',usercontroller.insertUser);
 
+//landing page route
+user_route.get('/',usercontroller.landingpage);
+
+
+//exporting routes
 module.exports = user_route;
